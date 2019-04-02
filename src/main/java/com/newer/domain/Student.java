02af072cd2b3e2ae19e87dd7df8897725ff24dac
ningbo1018 <1227566261@qq.com>
2019-04-02 14:36:55 +0800

    修改了c3p0的依赖项

---
+++ src/main/java/com/newer/domain/Student.java
@@ -0,0 +1,13 @@
+package com.newer.domain;
+
+import java.io.Serializable;
+
+/**
+ * @Author：ningbo
+ * @Date:2019/4/2
+ * @Description:com.newer.domain
+ * @Version:1.0
+ */
+public class Student implements Serializable {
+
+}
