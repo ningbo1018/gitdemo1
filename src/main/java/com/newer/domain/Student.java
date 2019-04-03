7db3dc4dab90981ab93d82aedaed74b7b908a135
ningbo1018 <1227566261@qq.com>
2019-04-03 11:22:43 +0800

    A同学修改学生类，增加了get,set方法

--- src/main/java/com/newer/domain/Student.java
+++ src/main/java/com/newer/domain/Student.java
@@ -9,6 +9,22 @@ import java.io.Serializable;
  * @Version:1.0
  */
 public class Student implements Serializable {
-  private Integer stuId;
+  private Integer stuid;
   private String stuname;
+
+  public Integer getStuid() {
+    return stuid;
+  }
+
+  public void setStuid(Integer stuid) {
+    this.stuid = stuid;
+  }
+
+  public String getStuname() {
+    return stuname;
+  }
+
+  public void setStuname(String stuname) {
+    this.stuname = stuname;
+  }
 }
